39009fbe91887a424aafb3c9874ec853c944d57b
MoisesGETIT <moisesescobarmartinez1@gmail.com>
2021-02-04 20:19:15 -0600

    adding the inventory of each product in the products module

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -18,6 +18,8 @@ export class ProductsComponent implements OnInit {
   tittleModal="";
   inventoryProducts: any =[];
   items: any = [];
+  inventoryProduct: any = [];
+
   constructor(
     private modalService: NgbModal,
     private formBuilder: FormBuilder,
@@ -49,15 +51,20 @@ export class ProductsComponent implements OnInit {
   }
 
   editProduct(modal, position){
-    this.tittleModal =" Crear un nuevo producto";
+    this.tittleModal =" Actualizar un producto";
     this.update = true
     this.positionItem = position
-    let inventoryProduct = this.items[position]
-    this.formProduct.get('name').setValue(inventoryProduct.name)
-    this.formProduct.get('description').setValue(inventoryProduct.description)
+    let product = this.items[position]
+    this.formProduct.get('name').setValue(product.name)
+    this.formProduct.get('description').setValue(product.description)
     this.activeModal = this.modalService.open(modal, { size: 'xl' });
   }
-
+  detailsInventory(modal,position){
+    this.positionItem = position
+    let product = this.items[position]
+    this.inventoryProduct = this.inventoryByProduct(product.name);
+    this.activeModal = this.modalService.open(modal, { size: 'xl' });
+  }
   saveProductData(){
     this.items.push(this.formProduct.value)
     this.inventoryService.updateProducts(this.items);
@@ -84,5 +91,18 @@ export class ProductsComponent implements OnInit {
       element.total = total;
       element.inventoryOutputs = inventoryOutputs;
     });
+  }
+
+  inventoryByProduct(nameProduct){
+    let inventoryProduct :any = []
+    this.inventoryProducts.forEach(element => {
+      if(element.product==nameProduct){
+        inventoryProduct.push(element);
+      }
+    });
+    return inventoryProduct;
+  }
+  close(){
+    this.activeModal.close();
   }
 }
